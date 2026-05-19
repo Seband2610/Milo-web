0dacfa0a80a816f56fc2b3085768e32f1db5b538
Sebastian Arzuaga <sebastianarzuaga20@gmail.com>
2026-05-19 14:59:41 -0500

    Dia 1 Milo completo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,59 @@
-alert("Milo inventory system started");
+// alert("Inventario Milo ha iniciado");
+
+let inventario = [];
+
+let producto = {
+  nombre: "Arroz",
+  precio: 5000,
+  cantidad: 3,
+};
+
+inventario.push(producto);
+
+console.log(inventario);
+
+function mostrarInventario() {
+  console.log("Inventario:");
+
+  for (let i = 0; i < inventario.length; i++) {
+    console.log(
+      `Nombre: ${inventario[i].nombre} | Precio: ${inventario[i].precio} | Cantidad: ${inventario[i].cantidad}`,
+    );
+  }
+}
+mostrarInventario();
+
+function agregarProducto() {
+  let nombre = prompt("Ingrese el nombre del producto:");
+  let precio = parseFloat(prompt("Ingrese el precio del producto:"));
+  let cantidad = parseInt(prompt("Ingrese la cantidad del producto:"));
+
+  let nuevoProducto = {
+    nombre: nombre,
+    precio: precio,
+    cantidad: cantidad,
+  };
+
+  inventario.push(nuevoProducto);
+  mostrarInventario();
+}
+agregarProducto();
+
+function buscarProducto() {
+  let nombreBusqueda = prompt("Ingrese el nombre del producto a buscar:");
+  let encontrado = false;
+
+  for (let i = 0; i < inventario.length; i++) {
+    if (inventario[i].nombre.toLowerCase() === nombreBusqueda.toLowerCase()) {
+      console.log(
+        `Producto encontrado: Nombre: ${inventario[i].nombre} | Precio: ${inventario[i].precio} | Cantidad: ${inventario[i].cantidad}`,
+      );
+      encontrado = true;
+      return;
+    }
+  }
+
+  if (!encontrado) {
+    console.log("Producto no encontrado.");
+  }
+}
